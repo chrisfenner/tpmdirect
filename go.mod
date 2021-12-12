6db2026a2393b6cda665346d89d00ed4bcdd9d34
Chris Fenner <commits@dlp.rip>
2021-12-11 18:31:16 -0800

    use fork of x/crypto

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/chrisfenner/tpmdirect
 go 1.16
 
 require (
+	github.com/chrisfenner/crypto v0.0.0-20211212022239-a128b7749a60
 	github.com/chrisfenner/go-tpm-sim v0.0.0-20211022232009-8a050aba42b1
 	github.com/google/go-cmp v0.5.6
 )
+
+replace golang.org/x/crypto => github.com/chrisfenner/crypto v0.0.0-20211212021344-34055e1e84a5
--- tpm2/kdf.go
+++ tpm2/kdf.go
@@ -3,26 +3,18 @@ package tpm2
 import (
 	"crypto/hmac"
 	"encoding/binary"
+	"hash"
+
+	"github.com/chrisfenner/crypto/kbkdf"
 )
 
 // KDFA implements the SP800-108A-CTR KDF as decribed in Part 1, 11.4.10
 func KDFA(alg TPMIAlgHash, key, label, contextU, contextV []byte, lenBytes int) []byte {
-	result := make([]byte, 0, lenBytes)
-	hashLen := alg.Hash().Size()
-	iterations := (lenBytes + hashLen - 1) / hashLen
-	for ctr := uint32(1); ctr <= uint32(iterations); ctr++ {
-		mac := hmac.New(alg.Hash, key)
-		binary.Write(mac, binary.BigEndian, ctr)
-		mac.Write(label)
-		if len(label) < 1 || label[len(label)-1] != 0x00 {
-			mac.Write([]byte{0x00})
-		}
-		mac.Write(contextU)
-		mac.Write(contextV)
-		binary.Write(mac, binary.BigEndian, uint32(lenBytes*8))
-		result = mac.Sum(result)
-	}
-	return result[:lenBytes]
+	context := make([]byte, 0, len(contextU)+len(contextV))
+	context = append(context, contextU...)
+	context = append(context, contextV...)
+	hmac := func() hash.Hash { return hmac.New(alg.Hash, key) }
+	return kbkdf.Counter(hmac, lenBytes, label, context)
 }
 
 // KDFe implements the SP800-56A KDF as decribed in Part 1, 11.4.10.3
